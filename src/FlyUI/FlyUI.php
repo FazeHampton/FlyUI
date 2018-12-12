98b1c53ddbcffb68fffc6a58d413fe044ad1be50
FazeHampton <45774514+FazeHampton@users.noreply.github.com>
2018-12-12 13:42:21 -0500

    Update FlyUI.php

--- src/FlyUI/FlyUI.php
+++ src/FlyUI/FlyUI.php
@@ -11,11 +11,11 @@ use pocketmine\event\server\ServerCommandEvent;
 class FlyUI extends PluginBase implements Listener{
     public function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents(($this), $this);
-        $this->getLogger()->info("Plugin Enabled By FazeHampton");
+        $this->getLogger()->info("FlyUI Plugin Enabled By FazeHampton");
     }
     public function onDisable(): void{
         $this->getServer()->getPluginManager()->registerEvents(($this), $this);
-        $this->getLogger()->info("Plugin Disabled By FazeHampton");
+        $this->getLogger()->info("FlyUI Plugin Disabled By FazeHampton");
     }
     public function checkDepends(): void{
         $this->formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
@@ -24,12 +24,20 @@ class FlyUI extends PluginBase implements Listener{
             $this->getPluginLoader()->disablePlugin($this);
         }
     }
+    public function onEntityDamage(EntityDamageEvent $event) {
+        if($event instanceof EntityDamageByEntityEvent) {
+            $damager = $event->getDamager();
+            if($damager instanceof Player && $this->isPlayer($damager)) {
+                $damager->sendMessage("You Cannot Damage A Player In FlyUI");
+                $event->setCancelled(true);
+            }
+        }
+    }
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
         if($cmd->getName() == "flyui"){
-            if(!($sender instanceof Player)){
-                $sender->sendMessage("FlyUI By FazeHampton", false);
+            if(!($sender instanceof Player))A{
+                $sender->sendMessage("", false);
                 return true;
-            }
             $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
             $form = $api->createSimpleForm(function (Player $sender, $data){
                 $result = $data;
@@ -37,22 +45,34 @@ class FlyUI extends PluginBase implements Listener{
                 }
                 switch ($result) {
                     case 0:
-                        $sender->sendMessage("FlyUI By MikeyHampton");
+                        $sender->sendMessage("FlyUI Enabled");
+                        $addTitle->("FlyUI Enabled");
+                        $sendTip->("FlyUI Enabled");
+                        $sendPopup->("FlyUI Enabled");
+                        $sender->setAllowFlight(false);
                         break;
                     case 1:
-                        $sender->sendMessage("Fly Enabled By FazeHampton");
-                        $this->getServer()->getCommandMap()->dispatch($player, "fly");
+                        $sender->sendMessage("FlyUI Disabled");
+                        $addTitle->("FlyUI Disabled");
+                        $sendTip->("FlyUI Disabled");
+                        $sendPopup->("FlyUI Disabled");
+                        $sender->setAllowFlight(true);
                         break;
                     case 2:
-                        $sender->sendMessage("Fly Disabled By FazeHampton");
-                        $this->getServer()->getCommandMap()->dispatch($player, "fly");
+                        $sender->sendMessage("FlyUI Error");
+                        $addTitle->("FlyUI Error");
+                        $sendTip->("FlyUI Error");
+                        $sendPopup->("FlyUI Error");
+                        $sender->setAllowFlight(true);
                         break;
                 }
             });
-            $form->setTitle("FlyUI By FazeHampton");
-            $form->setContent("FlyUI By FazeHampton");
+            $form->setTitle("FlyUI Settings");
+            $form->setContent("Below Allows You To Fly");
             $form->addButton("Fly Enable");
+            $form->setContent("Below Allows You To Disable Fly");
             $form->addButton("Fly Disable");
+            $form->setContent("Below Exits The FlyUI");
             $form->addButton("Exit");
             $form->sendToPlayer($sender);
         }
